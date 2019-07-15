506dc51c1888ef163f4d718867885cc130b63bed
Wilson Tan <49806519+wilsontwm@users.noreply.github.com>
2019-07-15 23:53:14 +0800

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -8,6 +8,6 @@ apt-get install mercurial git gcc libc6-dev -y
 
 # install golang
 cd /home/ec2-user
-hg clone -u release https://code.google.com/p/go
+hg clone -u release https://github.com/golang/go
 cd go/src
 ./all.bash
